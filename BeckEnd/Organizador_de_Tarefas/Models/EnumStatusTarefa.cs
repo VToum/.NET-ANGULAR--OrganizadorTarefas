5ad91281032828edcfcb72926af99344e2511817
Everton Leão <everleao@gmail.com>
2023-12-17 11:54:31 -0300

    Organizador de Tarefas - BeckEnd / .NET6 / SQL SERVE / FrontEnd / ANGULAR / BOOTSTRAP

---
+++ BeckEnd/Organizador_de_Tarefas/Models/EnumStatusTarefa.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Organizador_de_Tarefas.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente = 0,
+        Finalizado = 1
+    }
+}
---
+++ BeckEnd/Organizador_de_Tarefas/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Organizador_de_Tarefas.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adicionar Uma conexão com o banco
+
+builder.Services.AddDbContext<TarefaContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoTarefa")));
+
+builder.Services.AddCors();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
